938e133d74cd479fefe7a44cae1a8963fb25aac6
Juan Cassius <cassjuan@hotmail.com>
2023-04-27 09:11:30 -0300

    Exercícios finalizados
    Prontos para envio para a aplicação da vaga.

--- application.js
+++ application.js
@@ -69,8 +69,7 @@ const number4 = {
 //     4 - Dois veículos (um carro e um caminhão) saem respectivamente de cidades opostas pela mesma rodovia. 
 //     O carro de Ribeirão Preto em direção a Franca, a uma velocidade constante de 110 km/h e o caminhão de Franca em direção a Ribeirão Preto a uma velocidade constante de 80 km/h.
 //     Quando eles se cruzarem na rodovia, qual estará mais próximo a cidade de Ribeirão Preto?
-//     R: Podemos desconsiderar qualquer informação além de que eles se cruzam na rodovia. No momento em que se cruzam, os dois estarão a mesma distância de Ribeirão Preto.
-
+    
 
 // IMPORTANTE:
 
@@ -80,6 +79,8 @@ const number4 = {
 
 // c) Explique como chegou no resultado.
 
+//   RESPOSTA: Podemos desconsiderar qualquer informação além de que eles se cruzam na rodovia. No momento em que se cruzam, os dois estarão a mesma distância de Ribeirão Preto  
+  
 }
 
 const number5 = (string) => {
